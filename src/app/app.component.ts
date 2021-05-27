7771c8cdf5e6e316a0342ca623267f43739b4750
vipra ramtekkar <vipra.r@gmail.com>
2021-05-27 11:43:17 +0200

    changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Recipes';
+  title = 'Burger Place';
     constructor(private router: Router) {
 
     }
